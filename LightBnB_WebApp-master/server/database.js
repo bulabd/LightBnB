0cad33adad2f843500ab12dbbd298a009f1eb388
bulabd <bulabd@outlook.com>
2022-05-05 18:10:00 +0000

    Implement addProperty function as a query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -101,12 +101,7 @@ exports.addUser = addUser;
 const getAllReservations = function(guest_id, limit = 10) {
   // return getAllProperties(null, 2);
   const id = guest_id;
-  let max;
-  if (limit > 10 || limit < 0) {
-    max = null;
-  } else {
-    max = limit || 10;
-  }
+  const max = limit;
   const values = [id, max];
   return pool
     .query(`
@@ -179,9 +174,65 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const owner = property.owner_id;
+  const titre = property.title;
+  const desc = property.description;
+  const thumbnail = property.thumbnail_photo_url;
+  const cover = property.cover_photo_url;
+  const cost = property.cost_per_night * 100;
+  const rue = property.street;
+  const ville = property.city;
+  const prov = property.province;
+  const postcode = property.post_code;
+  const pay = property.country;
+  const parking = property.parking_spaces;
+  const bathrooms = property.number_of_bathrooms;
+  const bedrooms = property.number_of_bedrooms;
+  const values = [
+    owner,
+    titre,
+    desc,
+    thumbnail,
+    cover,
+    cost,
+    rue,
+    ville,
+    prov,
+    postcode,
+    pay,
+    parking,
+    bathrooms,
+    bedrooms
+  ];
+
+  return pool
+    .query(`
+      INSERT INTO properties (
+        owner_id, 
+        title, 
+        description, 
+        thumbnail_photo_url, 
+        cover_photo_url, 
+        cost_per_night,
+        street, 
+        city, 
+        province, 
+        post_code,
+        country,  
+        parking_spaces, 
+        number_of_bathrooms, 
+        number_of_bedrooms
+      )
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+    `, values)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 }
 exports.addProperty = addProperty;
